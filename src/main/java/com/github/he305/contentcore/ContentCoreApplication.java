e88a19e5ccd66a4646cdd7eda629cf2d0cebeb03
sergey <he305@mail.ru>
2022-07-31 14:51:29 +0300

    Initial commit

---
+++ src/main/java/com/github/he305/contentcore/ContentCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/he305/contentcore/ContentCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
