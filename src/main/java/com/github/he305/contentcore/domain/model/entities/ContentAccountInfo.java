47194a0df6cfceb4e7b1cee07635debf3aeeb35c
sergey <he305@mail.ru>
2022-07-31 23:09:18 +0300

    Added domain layer for ContentCreator

---
+++ src/main/java/com/github/he305/contentcore/domain/model/entities/ContentAccountInfo.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentcore.domain.model.entities;
+
+public class ContentAccountInfo {
+    private final String name;
+
+    public ContentAccountInfo(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/com/github/he305/contentcore/domain/model/enums/ContentType.java
@@ -0,0 +1,7 @@
+package com.github.he305.contentcore.domain.model.enums;
+
+public enum ContentType {
+    TWITCH,
+    WASD,
+    VK
+}
