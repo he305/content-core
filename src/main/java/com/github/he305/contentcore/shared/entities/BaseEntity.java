a3fc5e2fe581606c0402c4cc2650c61190d8e5b2
Sergey <he305@mail.ru>
2022-08-03 00:31:21 +0300

    #none Dev (#3)
    * Added jpa, swagger, h2
    
    * Added domain layer for ContentCreator
    
    * Added StringValidator
    
    * Added fields for swagger and h2 connect test
    
    * Added infrastructure layer for ContentCreator
    
    * Added application layer for ContentCreator
    
    * Tests started
    
    * Added DomainArchitectureTest

---
+++ src/main/java/com/github/he305/contentcore/shared/entities/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.github.he305.contentcore.shared.entities;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public abstract class BaseEntity {
+    protected UUID id = UUID.randomUUID();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
