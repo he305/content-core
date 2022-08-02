b0cb83c3b80ab942cd6c3ed8a4deec24e83308b7
sergey <he305@mail.ru>
2022-08-03 02:14:36 +0300

    Added shared exceptions

---
+++ src/main/java/com/github/he305/contentcore/shared/exceptions/ContentCoreException.java
@@ -0,0 +1,7 @@
+package com.github.he305.contentcore.shared.exceptions;
+
+public class ContentCoreException extends RuntimeException {
+    public ContentCoreException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/github/he305/contentcore/shared/validators/StringValidator.java
+++ src/main/java/com/github/he305/contentcore/shared/validators/StringValidator.java
@@ -1,12 +1,14 @@
 package com.github.he305.contentcore.shared.validators;
 
+import com.github.he305.contentcore.shared.exceptions.StringInvalidException;
+
 public class StringValidator {
     private StringValidator() {
     }
 
     public static String isNullOrEmpty(String instance) {
         if (instance == null || instance.isEmpty()) {
-            throw new IllegalArgumentException();
+            throw new StringInvalidException();
         }
         return instance;
     }
