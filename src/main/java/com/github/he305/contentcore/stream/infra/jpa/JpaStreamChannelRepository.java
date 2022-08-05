a5b62c57783d438d1cab7073c104b0ea8b5c19aa
sergey <he305@mail.ru>
2022-08-06 00:31:35 +0300

    Added findByStatus func

--- src/main/java/com/github/he305/contentcore/stream/infra/jpa/JpaStreamChannelRepository.java
+++ src/main/java/com/github/he305/contentcore/stream/infra/jpa/JpaStreamChannelRepository.java
@@ -1,13 +1,17 @@
 package com.github.he305.contentcore.stream.infra.jpa;
 
+import com.github.he305.contentcore.stream.domain.model.enums.StreamChannelStatus;
 import com.github.he305.contentcore.stream.infra.data.StreamChannelJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface JpaStreamChannelRepository extends JpaRepository<StreamChannelJpa, UUID> {
     Optional<StreamChannelJpa> findByContentAccountId(UUID id);
+
+    List<StreamChannelJpa> findByStatus(StreamChannelStatus status);
 }
