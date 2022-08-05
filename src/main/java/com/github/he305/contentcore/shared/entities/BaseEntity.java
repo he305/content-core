26f27bf0e13775e94da46454e6db15ae4aa367ca
sergey <he305@mail.ru>
2022-08-05 22:11:35 +0300

    Equals func changed

--- src/main/java/com/github/he305/contentcore/shared/entities/BaseEntity.java
+++ src/main/java/com/github/he305/contentcore/shared/entities/BaseEntity.java
@@ -1,24 +1,27 @@
 package com.github.he305.contentcore.shared.entities;
 
 import lombok.Getter;
+import lombok.NonNull;
 
-import java.util.Objects;
 import java.util.UUID;
 
 @Getter
 public abstract class BaseEntity {
+    @NonNull
     protected UUID id = UUID.randomUUID();
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof BaseEntity)) return false;
+
         BaseEntity that = (BaseEntity) o;
+
         return id.equals(that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return id.hashCode();
     }
 }
