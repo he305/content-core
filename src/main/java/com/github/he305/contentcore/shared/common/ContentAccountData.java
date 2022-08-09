a0cc55618a49a9ea71726feebc1700e61b8a720b
sergey <he305@mail.ru>
2022-08-09 19:35:42 +0300

    Added field to event data

--- src/main/java/com/github/he305/contentcore/shared/common/ContentAccountData.java
+++ src/main/java/com/github/he305/contentcore/shared/common/ContentAccountData.java
@@ -3,8 +3,11 @@ package com.github.he305.contentcore.shared.common;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.UUID;
+
 @RequiredArgsConstructor
 @Getter
 public class ContentAccountData {
+    private final UUID contentAccountId;
     private final String stringData;
 }
