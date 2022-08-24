7061bf4df5d089012cab12f5d8d1f461d1d2dd4c
Sergey <he305@mail.ru>
2022-08-25 02:30:37 +0300

    CON-44 added Eureka client (#48) #patch
    * CON-44 added Eureka client
    
    * CON-44 added EUREKA_HOST env

--- src/main/java/com/github/he305/contentcore/ContentCoreApplication.java
+++ src/main/java/com/github/he305/contentcore/ContentCoreApplication.java
@@ -2,8 +2,10 @@ package com.github.he305.contentcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContentCoreApplication {
 
 	public static void main(String[] args) {
